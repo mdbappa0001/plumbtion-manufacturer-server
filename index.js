3f21bf02dbb8f123703a5faf56e36a54ab9b46cc
mdbappa0001 <abutanif366@gmail.com>
2022-07-30 11:26:42 +0600

    shiped api added

--- index.js
+++ index.js
@@ -134,6 +134,17 @@ async function run() {
                 res.send(result)
             })
 
+             //33 shipped
+        app.put('/all-order/order/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id;
+            const filter = { _id : ObjectId(id)};
+            const updateDoc = {
+                $set: { process: 'Shipped' }
+            };
+            const result = await ordersCollection.updateOne(filter, updateDoc)
+            res.send(result)
+        })
+
            // ***    Review        **//
 
         //21 get reviews 
