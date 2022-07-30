361b7587894fed87cee7a5169f86a137da4c1b90
mdbappa0001 <abutanif366@gmail.com>
2022-07-30 12:41:12 +0600

    error handle

--- index.js
+++ index.js
@@ -16,7 +16,7 @@ app.use(express.json())
 
 //5 
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.emoo7.mongodb.net/?retryWrites=true&w=majority`;
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akda9.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
 
 // console.log(uri);
