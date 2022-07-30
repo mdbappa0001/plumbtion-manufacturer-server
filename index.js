38484d15d7b4e56920f35fbc405c00b5394d18ce
mdbappa0001 <abutanif366@gmail.com>
2022-07-30 11:28:22 +0600

    make and remove admin api added

--- index.js
+++ index.js
@@ -231,6 +231,22 @@ async function run() {
             res.send(result)
         })
 
+              // 19 check admin
+              app.get('/admin/:email', async (req, res) => {
+                const email = req.params.email
+                const user = await usersCollection.findOne({ email: email })
+                const isAdmin = user?.role === 'admin'
+                res.send({ admin: isAdmin })
+            })
+    
+            //20 delete user/admin
+            app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
+                const email = req.params.email
+                const filter = { email: email }
+                const result = await usersCollection.deleteOne(filter)
+                res.send(result)
+            })
+
 
 
            // ***    Review        **//
