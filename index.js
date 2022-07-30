dacf4f4b0ca25cd01c77e1ce5c25438c514efe0f
mdbappa0001 <abutanif366@gmail.com>
2022-07-30 11:31:46 +0600

    all api added

--- index.js
+++ index.js
@@ -1,8 +1,10 @@
 //1
 const express = require('express');
 const app = express()
-const { MongoClient, ServerApiVersion } = require('mongodb');
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const port = process.env.PORT || 5000
+const jwt = require('jsonwebtoken');
+const stripe = require('stripe')('sk_test_51L14pjDEsxnXfJbTlrS3grchkKNLNJquxxzz79aQiElQwp6RcnTeEJIRskV7INrmUt7vBTFS2pWMTokjKFP0nbIC00bPMze6Az')
 
 //3
 const cors = require('cors')
@@ -10,10 +12,14 @@ require('dotenv').config()
 
 //4
 app.use(cors())
-app.use(express.json());
+app.use(express.json())
 
-const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.akda9.mongodb.net/?retryWrites=true&w=majority`;
+//5 
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASSWORD}@cluster0.emoo7.mongodb.net/?retryWrites=true&w=majority`;
 const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+// console.log(uri);
 
 
 //14 jwt 
@@ -33,14 +39,47 @@ const verifyJWT = (req, res, next) => {
 }
 
 
+
+
+
+//6
 async function run() {
     try {
+        //7
         await client.connect();
         const toolsCollection = client.db("plumbtion-manufacturer").collection("tools");
         const reviewsCollection = client.db("plumbtion-manufacturer").collection("reviews");
         const ordersCollection = client.db("plumbtion-manufacturer").collection("orders");
         const usersCollection = client.db("plumbtion-manufacturer").collection("users");
         const paymentsCollection = client.db("plumbtion-manufacturer").collection("payments");
+
+
+        // 18 ( middleware ) verify admin 
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email
+            const requesterAccount = await usersCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next()
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden Access' });
+            }
+        }
+
+        //30 payment intent 
+        app.post('/create-payment-intent', verifyJWT, async (req, res) => {
+            const order = req.body;
+            const price = order.totalPrice;
+            const amount = price * 100;
+            const paymentIntent = await stripe.paymentIntents.create({
+                amount: amount,
+                currency: 'usd',
+                payment_method_types: ['card']
+            });
+            res.send({ clientSecret: paymentIntent.client_secret })
+        });
+
+        // ***    Tools (pipes)        **//
 
         //8 get tool 
         app.get('/tool', async (req, res) => {
@@ -49,16 +88,16 @@ async function run() {
             res.send(tools)
         })
 
-         //9 get single tool 
-         app.get('/tool/:id', async (req, res) => {
+        //9 get single tool 
+        app.get('/tool/:id', async (req, res) => {
             const id = req.params.id;
             const query = { _id: ObjectId(id) };
             const tool = await toolsCollection.findOne(query)
             res.send(tool)
         })
 
-          //23 add tool (pipes)
-          app.post('/tool', verifyJWT, verifyAdmin, async (req, res) => {
+        //23 add tool (pipes)
+        app.post('/tool', verifyJWT, verifyAdmin, async (req, res) => {
             const pipe = req.body
             const result = await toolsCollection.insertOne(pipe)
             res.send(result)
@@ -87,7 +126,9 @@ async function run() {
             res.send(result)
         })
 
-          // ***    Order        **//
+
+
+        // ***    Order        **//
 
         //10 get orders
         app.post('/order', async (req, res) => {
@@ -96,8 +137,8 @@ async function run() {
             res.send(result)
         })
 
-           //12 read my orders (get)
-           app.get('/order', verifyJWT, async (req, res) => {
+        //12 read my orders (get)
+        app.get('/order', verifyJWT, async (req, res) => {
             const email = req.query.email;
             const decodedEmail = req.decoded.email
             if (email === decodedEmail) {
@@ -126,31 +167,40 @@ async function run() {
             res.send(allOrder)
         })
 
-             //31 payment 
-             app.patch('/order/:id', verifyJWT, async (req, res) => {
-                const id = req.params.id;
-                const payment = req.body;
-                const filter = { _id: ObjectId(id) };
-                const updatedDoc = {
-                    $set: {
-                        paid: true,
-                        transactionId: payment.transactionId
-                    }
-                }
-                const result = await paymentsCollection.insertOne(payment);
-                const updatedBooking = await ordersCollection.updateOne(filter, updatedDoc);
-                res.send(updatedBooking,result);
-            })
-    
-            //32 delete order
-            app.delete('/all-order/:id', verifyJWT, verifyAdmin, async (req, res) => {
-                const id = req.params.id
-                const filter = { _id : ObjectId(id) }
-                const result = await ordersCollection.deleteOne(filter)
-                res.send(result)
-            })
+        // payment 
+        //29 get  order id 
+        app.get('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id
+            const query = { _id: ObjectId(id) }
+            const order = await ordersCollection.findOne(query)
+            res.send(order)
+        })
 
-             //33 shipped
+        //31 payment 
+        app.patch('/order/:id', verifyJWT, async (req, res) => {
+            const id = req.params.id;
+            const payment = req.body;
+            const filter = { _id: ObjectId(id) };
+            const updatedDoc = {
+                $set: {
+                    paid: true,
+                    transactionId: payment.transactionId
+                }
+            }
+            const result = await paymentsCollection.insertOne(payment);
+            const updatedBooking = await ordersCollection.updateOne(filter, updatedDoc);
+            res.send(updatedBooking,result);
+        })
+
+        //32 delete order
+        app.delete('/all-order/:id', verifyJWT, verifyAdmin, async (req, res) => {
+            const id = req.params.id
+            const filter = { _id : ObjectId(id) }
+            const result = await ordersCollection.deleteOne(filter)
+            res.send(result)
+        })
+
+        //33 shipped
         app.put('/all-order/order/:id', verifyJWT, verifyAdmin, async (req, res) => {
             const id = req.params.id;
             const filter = { _id : ObjectId(id)};
@@ -163,7 +213,8 @@ async function run() {
 
 
 
-           // ***    User        **//
+
+        // ***    User        **//
 
         //15  user create or update 
         app.put('/user/:email', async (req, res) => {
@@ -247,24 +298,24 @@ async function run() {
             res.send(result)
         })
 
-              // 19 check admin
-              app.get('/admin/:email', async (req, res) => {
-                const email = req.params.email
-                const user = await usersCollection.findOne({ email: email })
-                const isAdmin = user?.role === 'admin'
-                res.send({ admin: isAdmin })
-            })
-    
-            //20 delete user/admin
-            app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
-                const email = req.params.email
-                const filter = { email: email }
-                const result = await usersCollection.deleteOne(filter)
-                res.send(result)
-            })
+        // 19 check admin
+        app.get('/admin/:email', async (req, res) => {
+            const email = req.params.email
+            const user = await usersCollection.findOne({ email: email })
+            const isAdmin = user?.role === 'admin'
+            res.send({ admin: isAdmin })
+        })
+
+        //20 delete user/admin
+        app.delete('/user/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email
+            const filter = { email: email }
+            const result = await usersCollection.deleteOne(filter)
+            res.send(result)
+        })
 
 
-    // ***    Review        **//
+        // ***    Review        **//
 
         //21 get reviews 
         app.get('/review', async (req, res) => {
@@ -291,17 +342,17 @@ async function run() {
 
     }
     finally {
-
+        //   await client.close();
     }
 }
+run().catch(console.dir);
 
-run().catch(console.dir)
 
-
+//2
 app.get('/', (req, res) => {
-    res.send('Hello From Plumbkin Manufacture')
+    res.send('Plumbtion server is running')
 })
 
 app.listen(port, () => {
-    console.log(`Plumbkin Manufacture Listening On Port ${port}`);
+    console.log('server running on ', port);
 })
